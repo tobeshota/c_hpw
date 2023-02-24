7ad50cc852844141cb250108aa71196621b6bc7a
tobeshota <Shota.Tobe@keio.jp>
2023-02-24 12:31:00 +0900

    int Myfunc(int, int)

--- 1p24.c
+++ 1p24.c
@@ -1,9 +1,9 @@
 /* 変数aに123、変数bに456という値を代入してから、それらを引数（パラメータ）に与えてMyFuncとい関数を呼び出す */
 #include <stdio.h>
 
-void MyFunc(int *a, int *b)
+int MyFunc(int a, int b)
 {
-    *a += *b;
+    return a + b;
 }
 
 int main(void)
@@ -11,9 +11,9 @@ int main(void)
     int a = 123;
     int b = 456;
 
-    MyFunc(&a, &b);
+    int c = MyFunc(a, b);
 
-    printf("%d", a);
+    printf("%d", c);
 
     return 0;
 }
